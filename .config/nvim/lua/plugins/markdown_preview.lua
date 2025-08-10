be5e2df9400ee3e574357906842f12ad8f771b8c
blru <44450511+blru@users.noreply.github.com>
2025-08-10 17:29:55 -0400

    fix(nvim): made markdown preview properly reflect switching from one file to another

--- .config/nvim/lua/plugins/markdown_preview.lua
+++ .config/nvim/lua/plugins/markdown_preview.lua
@@ -7,7 +7,8 @@ return {
         vim.g.mkdp_port = "8090"
         vim.g.mkdp_theme = "dark"
         vim.g.mkdp_auto_close = 0
-        vim.g.mkdp_combine_preview = 0
+        vim.g.mkdp_combine_preview = 1
+        vim.g.mkdp_combine_preview_auto_refresh = 1
     end,
     ft = { "markdown" },
 }
