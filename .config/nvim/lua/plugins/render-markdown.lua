cce69ae1501f4c8b55a6f01aca430ec183ef97c9
blru <44450511+blru@users.noreply.github.com>
2025-08-19 10:23:37 -0400

    feat(nvim): added render-markdown

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,18 @@
+return {
+    "MeanderingProgrammer/render-markdown.nvim",
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons",
+    },
+    ft = { "markdown" },
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {
+        sign = { enabled = false },
+        html = {
+            comment = {
+                conceal = false,
+            },
+        },
+    },
+}
